a54f58d4346e572c1a4d5505c710ccf6c23dc64d
Abraham Qian <qianyonglian@outlook.com>
2020-09-15 17:51:00 +0800

    first commit

---
+++ IOSXAF/IOSXAF/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using IOSXAF.ViewModels;
+
+namespace IOSXAF.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
